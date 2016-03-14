e76a7d6a64f30cf1a93018d4e8328af3040ef232
Chad <spizeck@zoho.com>
2016-03-13 22:09:33 -0800

    Organized templates directory. Added error handling 404 and a 500 debugging template.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template
 
+from content_management import Content
+
+TOPIC_DICT = Content()
+
 app = Flask(__name__) 
 
 @app.route('/')
@@ -10,5 +14,19 @@ def homepage():
 def dashboard():
     return render_template("dashboard.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("errors/404.html")
+
 if __name__ == '__main__':
     app.run()
+
+"""
+Helpful debugging code for new page
+@app.rout('/example')
+def example():
+    try:
+        return render_template("example.html", VARIABLE_TO_BE_PASSED = VARIABLE)
+    except Exception as e:
+        return render_template("500.html", error = e)
+"""
---
+++ content_management.py
@@ -0,0 +1,8 @@
+
+def Content():
+    TOPIC_DICT = {"testdict":[['test', 'result'],
+                          ['test2', 'result2']],
+                  'testdict2': [['foo', 'bar'],
+                                'foo2', 'bar2']}
+
+    return TOPIC_DICT
