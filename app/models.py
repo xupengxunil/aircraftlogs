2ccdcf52bb60acf2a9171a8bf3819ce87ec03731
Chad <spizeck@zoho.com>
2016-03-25 23:40:19 -0800

    Major update to app structure. Commented out MySQL stuff and got SQLite db up.
    Started integration for Flask-SQLAlchemy.
    
    Major major restructuring, moved everything out of __init__.py into models.py and views.py.
    
    Not a functioning version, login / register pages are currently broken. Need integration of SQLAlchemy.

--- app/models.py
+++ app/models.py
@@ -1,6 +1,24 @@
 from wtforms import Form, BooleanField, StringField, PasswordField
 from wtforms.validators import Length, DataRequired, EqualTo
 
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), index=True, unique=True)
+    first_name = db.Column(db.String(50), index=True)
+    last_name = db.Column(db.String(50), index=True)
+    email = db.Column(db.String(100), index=True, unique=True)
+
+    def __init__(self, username, first_name, email):
+        self.username = username
+        self.first_name = first_name
+        self.email = email
+
+    def __repr__(self):
+        return '<User %r>' % self.first_name
+
 
 class RegistrationForm(Form):
     username = StringField('Username', [Length(min=4, max=20)])
